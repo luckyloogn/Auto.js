fb766b925b23b67c4eab85df2ecebe49ec4ee53c
lucky9loogn <lucky9loogn@gmail.com>
2024-10-27 15:48:53 +0800

    inrt(template.apk)的菜单栏添加“清除日志”和“退出”选项

--- inrt/src/main/java/com/stardust/autojs/inrt/LogActivity.kt
+++ inrt/src/main/java/com/stardust/autojs/inrt/LogActivity.kt
@@ -2,18 +2,21 @@ package com.stardust.autojs.inrt
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import com.stardust.autojs.core.console.ConsoleImpl
+import com.stardust.autojs.core.console.ConsoleView
 import com.stardust.autojs.inrt.autojs.AutoJs
 import com.stardust.autojs.inrt.launch.GlobalProjectLauncher
-import com.stardust.autojs.core.console.ConsoleView
-import com.stardust.autojs.core.console.ConsoleImpl
+import kotlin.system.exitProcess
 
 class LogActivity : AppCompatActivity() {
+
+    private lateinit var mConsoleImpl: ConsoleImpl
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,24 +30,48 @@ class LogActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        mConsoleImpl = AutoJs.instance.globalConsole as ConsoleImpl
+
         val consoleView = findViewById<ConsoleView>(R.id.console)
-        consoleView.setConsole(AutoJs.instance.globalConsole as ConsoleImpl)
+        consoleView.setConsole(mConsoleImpl)
         consoleView.findViewById<View>(R.id.input_container).visibility = View.GONE
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        startActivity(Intent(this, SettingsActivity::class.java))
-        return true
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.action_exit -> {
+                exitProcess(0)   // 完全退出程序
+            }
+            R.id.action_clear_log -> {
+                mConsoleImpl.clear()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
+        // 让三个点图标（即溢出菜单）的颜色和其他图标颜色一致
+        val iconColor = ContextCompat.getColor(this, android.R.color.white)
+        for (i in 0 until menu.size()) {
+            val menuItem = menu.getItem(i)
+            val drawable = menuItem.icon
+            drawable?.let {
+                it.setTint(iconColor)
+                menuItem.icon = it
+            }
+        }
         return true
     }
 
     companion object {
-
-
-        val EXTRA_LAUNCH_SCRIPT = "launch_script"
+        const val EXTRA_LAUNCH_SCRIPT = "launch_script"
     }
 }
